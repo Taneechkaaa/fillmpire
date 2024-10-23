978eed83be623ca61086c6d372cf4506ab8d7f03
Tanya Bondarenko <taneechkaaa@mail.ru>
2024-10-23 12:26:30 +0700

    Добавила Componets

--- src/componets/App.tsx
+++ src/componets/App.tsx
@@ -1,15 +1,18 @@
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./index";
+
 const App = () => {
   return (
     <div>
+      {/* <NavBar /> */}
       <main>
         <Routes>
-          <Route path="/movie/:id" element={<h1>Movie Information</h1>}></Route>
-          <Route path="/actors/:id" element={<h1>Actors</h1>}></Route>
-          <Route path="/movies" element={<h1>Movies</h1>}></Route>
-          <Route path="/profile/:id" element={<h1>Profile</h1>}></Route>
+          <Route path="/movie/:id" element={<MovieInformation />}></Route>
+          <Route path="/actors/:id" element={<Actors />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
         </Routes>
       </main>
 
---
+++ src/componets/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  console.log("Navigation");
+  return (
+    <div>
+      <h1>Navigation</h1>
+    </div>
+  );
+};
+
+export default NavBar;
